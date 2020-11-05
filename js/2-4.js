0b66551e71bbd308bf24d333c45e27c1e34f4c63
Dmytro <mityaua@gmail.com>
2020-11-05 22:00:19 +0200

    Обновленое решение 4й задачи

--- js/2-4.js
+++ js/2-4.js
@@ -1,10 +1,10 @@
 function formatString(string, maxLength = 40) {
   // Write code under this line
-  if (string.length <= maxLength) {
-    return string;
-  } else {
-    return string.slice(0, maxLength) + "...";
+  if (string.length > maxLength) {
+    const shortString = string.slice(0, maxLength) + '...';
+    return shortString;
   }
+  return string;
 }
 
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
@@ -13,5 +13,13 @@ console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
 // 'Vestibulum facilisis, purus nec pulvinar...'
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+console.log(
+  formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30),
+);
 // 'Vestibulum facilisis, purus ne...'
+
+// 1. Возвращаем через фунцкию (по условию задачи) исходною строку string;
+// 2. Добавляем условие if;
+// 3. Условие - если длинна строки больше чем указано в параметре maxLength, тогда применяем обрезку;
+// 4. Обрезка - создали переменную shortString, куда записали строку с методом slice(0, maxLength) - от начала строки и до 40 символов;
+// 5. Возвращаем тут же переменную shortString;
