abbc0b6ddae0d305e1f73a60082d23d8d3803d27
Dmytro <mityaua@gmail.com>
2020-11-05 21:34:05 +0200

    Переписано выполнение 3й задачи

--- js/2-3.js
+++ js/2-3.js
@@ -1,22 +1,27 @@
-function findLongestWord(string = "") {
+function findLongestWord(string = '') {
   // Write code under this line
-  const stringSplit = string.split(" ");
-  let longestWord = "";
+  const words = string.split(' ');
+  let longestWord = '';
 
-  for (let word of stringSplit) {
+  for (let word of words) {
     if (word.length > longestWord.length) {
       longestWord = word;
     }
   }
-
   return longestWord;
 }
 
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
 // 'jumped'
 
-console.log(findLongestWord("Google do a roll"));
+console.log(findLongestWord('Google do a roll'));
 // 'Google'
 
-console.log(findLongestWord("May the force be with you"));
+console.log(findLongestWord('May the force be with you'));
 // 'force'
+
+// 1. Создали переменную words. В ней перевели получаемую строку string в массив
+// 2. Создали переменную для самого длинного слова longestWord (с пустой строкой)
+// 3. Используя цикл for of мы перебираем массив с условием if
+// 4. Условие - если длинна слова в массиве больше чем длинна переменной longestWord, тогда присваиваем найденное слово в longestWord
+// 5. Возвращаем через функцию переменную с самым длинным словом, то есть longestWord
