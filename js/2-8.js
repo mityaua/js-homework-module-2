1e95aa5a5abc77a30d6b5341f471e118158d73a6
Dmytro <mityaua@gmail.com>
2020-11-06 19:49:57 +0200

    Добавлено решение 7й задачи

---
+++ js/2-8.js
@@ -0,0 +1,21 @@
+// 1. Используем цикл для иттерации по массиву array
+// 2. На каждой иттерации записываем в total сумму всех значений из массива
+
+function reduceArray(array) {
+  'use strict';
+  let total = 0;
+  // Write code under this line
+  for (let i = 0; i < array.length; i += 1) {
+    total += array[i];
+  }
+  return total;
+}
+
+console.log(reduceArray([1, 2, 3]));
+// 6
+
+console.log(reduceArray([-2, 0, 2]));
+// 0
+
+console.log(reduceArray([1, 2, 2.5]));
+// 5.5
